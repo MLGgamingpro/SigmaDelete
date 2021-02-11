bf4afadadc5f6c9e441e5467d092f6dc7b3cd499
MLGgamingpro <69982467+MLGgamingpro@users.noreply.github.com>
2021-02-11 13:31:28 -0500

    Update SigmaDeleter.bat

--- SigmaDeleter.bat
+++ SigmaDeleter.bat
@@ -1,7 +1,13 @@
 @echo off
-del /s /q "C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*"
-for /d %%p in ("C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*" do rmdir "%%p" /s /q
+del /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*"
+for /d %%p in ("C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*" do rmdir "%%p" /s /q
 
-del /s /q "C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*"
-for /d %%p in ("C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*" do rmdir "%%p" /s /q
+del /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*"
+for /d %%p in ("C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*" do rmdir "%%p" /s /q
+
+del /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*youtube-dl.exe*"
+for /d %%p in ("C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*youtube-dl.exe*" do rmdir "%%p" /s /q
+
+del /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*JelloPrelauncher.jar*"
+for /d %%p in ("C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*JelloPrelauncher.jar*
 exit
